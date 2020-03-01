b7af16bdd62b4cafc03f820ba23d32233c5f7b45
adjacentech_suntao <suntao@adjacentech.com>
2020-03-01 10:38:31 +0800

    添加Spring Boot OAuth2认证服务器Demo

---
+++ springboot-oauth2-authserver/src/main/java/com/opfly/demo/config/AuthorizationServerConfig.java
@@ -0,0 +1,93 @@
+package com.opfly.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	private static final String DEMO_RESOURCE_ID = "demo";
+	
+	@Autowired
+    AuthenticationManager authenticationManager;
+	@Autowired
+    PasswordEncoder passwordEncoder;
+	
+	@Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    	String finalSecret = passwordEncoder.encode("123456");
+        //配置两个客户端,一个用于password认证一个用于client认证
+    	clients.inMemory()
+				.withClient("client01")
+		        .resourceIds(DEMO_RESOURCE_ID)
+		        .authorizedGrantTypes("password", "refresh_token")
+		        .scopes("api")
+		        .secret(finalSecret)
+		        .accessTokenValiditySeconds(60 * 60 * 8)
+		        .refreshTokenValiditySeconds(60 * 60 * 24 * 7)
+		        .and()
+		        .withClient("client02")
+		        .resourceIds(DEMO_RESOURCE_ID)
+		        .authorizedGrantTypes("client_credentials")
+		        .scopes("api")
+		        .secret(finalSecret)
+		        .accessTokenValiditySeconds(60 * 60 * 8)
+		        .refreshTokenValiditySeconds(60 * 60 * 24 * 7);
+    }
+	
+	/*
+	 * 将token保存在内存中
+	 */
+	/*//使用Reference Token
+	public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+    
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    	endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager);
+    }*/
+	
+	//使用Json Web Token
+	@Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+    
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("111111");
+        return converter;
+    }
+    
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    	endpoints
+                .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager);
+    }
+	
+	@Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+    	oauthServer
+        .tokenKeyAccess("permitAll()")
+        .checkTokenAccess("permitAll()")
+        .allowFormAuthenticationForClients();
+    }
+}
---
+++ springboot-oauth2-authserver/src/main/java/com/opfly/demo/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.opfly.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	/*
+	 * 重写userDetailsService，在内存中添加测试用户
+	 */
+	@Bean
+    @Override
+    protected UserDetailsService userDetailsService(){
+        String finalPassword = passwordEncoder().encode("123456");
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user_1").password(finalPassword).authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password(finalPassword).authorities("USER").build());
+        return manager;
+    }
+	
+	/*
+	 * 进行密码加密
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	/*
+	 * 重写authenticationManagerBean，并注册到容器中
+	 */
+	@Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        AuthenticationManager manager = super.authenticationManagerBean();
+        return manager;
+    }
+	
+	/*
+	 * 重写configure，配置访问策略
+	 */
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().anyRequest()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/oauth/*").permitAll();
+    }
+}
